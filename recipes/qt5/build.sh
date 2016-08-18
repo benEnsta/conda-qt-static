03131f7cc3be0b166eaa2d338ae48422144d5190
Benoit DESROCHERS <ben.ensta@gmail.com>
2016-08-18 11:01:43 +0200

    [master] update scripts

--- recipes/qt5/build.sh
+++ recipes/qt5/build.sh
@@ -1,17 +1,31 @@
 #!/bin/bash
 
-echo $(gcc --version)
+
 tar xzf ./qt-lite.tar.gz
 
 cd qt-everywhere-opensource-src-5.7.0
 mkdir build
 cd build
 
-../configure -release -opensource -static \
-             -confirm-license -c++std c++11  \
+if [ "$TRAVIS_OS_NAME" = "osx" ]; then
+    ../configure -release -opensource -static \
+             -confirm-license  \
              -no-mtdev -no-journald \
              -nomake examples -nomake tests \
-             -qt-xcb --prefix=${CONDA_PREFIX}
+             --prefix=${CONDA_PREFIX} \
+             -sdk 10.11;
+fi
+
+if [ "$TRAVIS_OS_NAME" = "linux" ]; then
+  alias gcc="gcc-4.9"
+  alias g++="g++-4.9"
+  echo $($CC --version)s
+  ../configure -release -opensource -static \
+               -confirm-license -c++std c++11 \
+               -no-mtdev -no-journald \
+               -nomake examples -nomake tests \
+               -qt-xcb --prefix=${CONDA_PREFIX};
+fi
 
 #-gtkstyle -opengl -qt-freetype
 make -j2
