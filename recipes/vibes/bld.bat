c48dae8cf41b9ab5c931ed1d494cb21baeb5ad89
Benoit DESROCHERS <ben.ensta@gmail.com>
2016-08-20 17:39:10 +0200

    [master] add vibes receipe

---
+++ recipes/vibes/bld.bat
@@ -0,0 +1,16 @@
+cd viewer
+
+@ECHO ON
+
+call "%CONDA_PREFIX%"\bin\qmake.exe PREFIX="%CONDA_PREFIX%"
+
+call nmake
+
+call nmake install
+if errorlevel 1 exit 1
+
+:: Add more build steps here, if they are necessary.
+
+:: See
+:: http://docs.continuum.io/conda/build.html
+:: for a list of environment variables that are set during the build process.
