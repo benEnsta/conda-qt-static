9d38e1b9faea004f53b777a0bb638ce8d3de7d3c
Benoit DESROCHERS <ben.ensta@gmail.com>
2016-08-21 12:18:01 +0200

    [master] try linux build

--- recipes/qt5/build.sh
+++ recipes/qt5/build.sh
@@ -4,28 +4,25 @@
 # tar xzf ./qt-lite.tar.gz
 #
 # cd qt-everywhere-opensource-src-5.7.0
-mkdir -p build
 
-cd build
-
+# export TRAVIS_OS_NAME=linux
 if [ "$TRAVIS_OS_NAME" = "osx" ]; then
-    ../configure -release -opensource -static \
+    ./configure -release -opensource -static \
              -confirm-license  \
              -no-mtdev -no-journald \
              -nomake examples -nomake tests \
              --prefix=${CONDA_PREFIX} \
-             -sdk 10.11;
+             -platform macx-clang;
 fi
 
 if [ "$TRAVIS_OS_NAME" = "linux" ]; then
-  alias gcc="gcc-4.9"
-  alias g++="g++-4.9"
-  echo $(gcc --version)
-  ../configure -release -opensource -static \
+  # tric to use gcc-4.9 compiller
+  # wget -L https://github.com/benEnsta/conda-qt-static/blob/master/recipes/qt5/g++-base.conf -O mkspec/common/g++-base.conf
+  ./configure -release -opensource -static \
                -confirm-license -c++std c++11 \
                -no-mtdev -no-journald \
                -nomake examples -nomake tests \
-               -qt-xcb --prefix=${CONDA_PREFIX};
+               -qt-xcb --prefix=${CONDA_PREFIX} -no-icu -v;
 fi
 
 #-gtkstyle -opengl -qt-freetype
